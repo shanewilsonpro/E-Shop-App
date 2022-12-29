33b634c91b996a4e95799ff3da64133338262301
Shane Wilson <shanewilsonpro@gmail.com>
2022-12-29 15:23:40 -0800

    working on backend for models and routes

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
+
+// middlewares
+app.use(express.json());
+app.use(morgan("tiny"));
+
+//Routes
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+// database
+mongoose
+  .connect(process.env.MONGOOSE_DB)
+  .then(() => {
+    console.log("Database connection is ready...");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// server
+app.listen(3000, () => {
+  console.log("The server is running...");
+});
